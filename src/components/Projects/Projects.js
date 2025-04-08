e8a99281a19c7f2b771325d32dc2552e599390d3
Allain Legaspi <alegaspi_ccs@uspf.edu.ph>
2025-04-09 00:30:14 +0800

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,15 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 import persoWebsite from "../../Assets/Projects/perso-website.png";
 import trading from "../../Assets/Projects/trading.png";
-import LIMS from "../../Assets/Projects/LIMS.png";  // Add this import
+import LIMS from "../../Assets/Projects/LIMS.png";  
 
 function Projects() {
   return (
